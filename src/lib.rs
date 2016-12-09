0d0447ab9c547b3fa44ae73d8b6e021a2371a106
Ashley Williams <ashley666ashley@gmail.com>
2016-12-09 11:37:33 -1000

    build out correct interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-#![deny(warnings)]
 extern crate futures;
 extern crate hyper;
 extern crate tokio_core;
@@ -7,41 +6,46 @@ use futures::Future;
 use futures::stream::Stream;
 
 use hyper::Client;
+use hyper::client::Response;
 
 use std::io::{self, Write};
 
+type Event = hyper::Chunk;
+
 pub struct ChangesStream {
-  db: String,
+  db: hyper::Url,
+  lp: tokio_core::reactor::Core,
+  handlers: Option<Vec<Box<Fn(&Event)>>>,
 }
 
 impl ChangesStream {
   pub fn new(db: String) -> ChangesStream {
     ChangesStream {
-      db: db,
+      db: db.parse().unwrap(),
+      lp: tokio_core::reactor::Core::new().unwrap(),
+      handlers: Some(vec![]),
     }
   }
 
-  pub fn db(&self) -> &String {
-    &self.db
+  pub fn on<F: Fn(&Event) + 'static>(&mut self, handler: F) {
+    self.handlers.as_mut().unwrap().push(Box::new(handler));
   }
 
-  pub fn run(&self) {
-    let mut core = tokio_core::reactor::Core::new().unwrap();
-    let client = Client::new(&core.handle()).unwrap();
+  pub fn run(mut self)  {
+    let client = Client::new(&self.lp.handle()).unwrap();
 
-    let url = &self.db();
+    let handlers = self.handlers.take().unwrap();
+        self.lp.run(client.get(self.db).and_then(move |res| {
+            assert!(res.status().is_success());
+            
+            res.body().for_each(move |chunk| {
+                for handler in &handlers {
+                    // let event = serde_json::decode(chunk);
+                    handler(&chunk);
+                }
+                Ok(())
+            })
+        })).unwrap();
 
-    let work = client.get(url.parse().unwrap()).and_then(|res| {
-        println!("Response: {}", res.status());
-        println!("Headers: \n{}", res.headers());
-
-        res.body().for_each(|chunk| {
-            io::stdout().write_all(&chunk).map_err(From::from)
-        })
-    }).map(|_| {
-        println!("\n\nDone.");
-    });
-
-    core.run(work).unwrap();
   }
 }
