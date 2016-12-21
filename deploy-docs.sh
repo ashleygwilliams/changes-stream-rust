3bff033fe6b45dc31a80e71236a48803a0d89956
Ashley Williams <ashley666ashley@gmail.com>
2016-12-21 16:12:06 -0500

    fix(deploy): i'm not good at this

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -5,6 +5,8 @@ set -o errexit -o nounset
 rev=$(git rev-parse --short HEAD)
 
 cargo doc
+cd target/doc
+echo "<meta http-equiv=refresh content=0;url=`echo $TRAVIS_REPO_SLUG | cut -d '/' -f 2`/index.html>" > target/doc/index.html
 
 git init
 git config user.name "Ashley Williams"
