0bc312417706cbb46001e4b7e23ab0c5adc58d72
Ashley Williams <ashley666ashley@gmail.com>
2016-12-21 15:38:49 -0500

    add travis deploy for docs

---
+++ deploy-docs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+rev=$(git rev-parse --short HEAD)
+
+cd target/doc/changes_stream
+
+git init
+git config user.name "Ashley Williams"
+git config user.email "ashley666ashley@gmail.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/ashleygwilliams/changes-stream-rust.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
